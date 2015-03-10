10da87d52628f8ed189252cb1edfa6a55cee3579
hezedu@hotmail.com <hezedu@hotmail.com>
2015-03-10 11:01:36 +0800

    出题：如何保证一个程序一直在执行？

--- isDie.js
+++ isDie.js
@@ -1,18 +1,23 @@
 var child_process = require('child_process');
 
-var exec_SH = 'ps -ef |grep "node" |grep -v "grep"';
-
+var appName = 'node';
+var exec_SH = 'ps -ef |grep "' + appName + '" |grep -v "grep"';
 var count = 0;
-function isDie(){
-var child = child_process.exec(exec_SH, function(err, stdout, stderr) {
-  count++;
-  if(err || !stdout || stderr){
-    console.log('die');
-    return;
-  }
-  console.log('live');
-  console.log('count=='+count);
-  setTimeout(isDie,6000);
-});
+
+function isDie() {
+  var child = child_process.exec(exec_SH, function(err, stdout, stderr) {
+    count++;
+    console.log('stdout.length=' + stdout.length);
+    if (err || !stdout || stderr) {
+      console.log(appName + ' is died');
+      return;
+    }
+
+    console.log(appName + ' is living');
+    console.log('count==' + count);
+    setTimeout(isDie, 6000);
+  });
+
 }
+
 isDie();
