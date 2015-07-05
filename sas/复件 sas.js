b7ec2313b73191500f51041a20cd5f0706856af4
hezedu <hezedu@hotmail.com>
2015-07-06 03:58:01 +0800

    2

---
+++ sas/复件 sas.js
@@ -0,0 +1,337 @@
+/*
+作者:hezedu
+home:https://godmod.cn
+https://github.com/hezedu/sas
+v:2.0
+License :mit
+2015/07/04
+*/
+
+//*******************************************************************************************************************
+function sas(tasks, opts, end) {
+  //参数样式: 根据参数个数 判定函数的 api样式
+  var ite;
+  if (typeof opts !== 'object') {
+    switch (arguments.length) {
+      case 2:
+        end = opts;
+        break;
+      case 3:
+        ite = opts;
+        break;
+      default:
+        opts = {};
+    }
+  } else {
+    ite = opts.iterator;
+    end = end || opts.allEnd;
+  }
+  //参数样式完
+  new sas.Min(tasks, ite, end, opts);
+}
+//主end
+//*******************************************************************************************************************
+
+//主静态方法
+//sas.typeArr = ['[object Function]', '[object Object]', '[object Array]'];
+sas.type = Object.prototype.toString;
+
+sas.ARR = '[object Array]';
+sas.FN = '[object Function]';
+sas.OBJ = '[object Object]';
+
+sas.copy = function(t) {
+  var c;
+  switch (sas.type.call(t)) {
+    case sas.OBJ: //obj
+      c = {};
+      for (var i in t) {
+        sas._copy(t, i, c);
+      }
+      break;
+    case sas.ARR: //arr
+      c = [], len = t.length;
+      for (var i = 0; i < len; i++) {
+        sas._copy(t, i, c);
+      }
+      break;
+    default:
+      c = t;
+  }
+  return c;
+}
+sas._copy = function(t, i, c) {
+    switch (sas.type.call(t[i])) {
+      case sas.OBJ: //obj
+        c[i] = {};
+        for (var j in t[i]) {
+          sas._copy(t[i], j, c[i]);
+        }
+        break;
+      case sas.ARR: //arr
+        c[i] = [], len = t[i].length;
+        for (var j = 0; j < len; j++) {
+          sas._copy(t[i], j, c[i]);
+        }
+        break;
+      default:
+        c[i] = t[i];
+    }
+  }
+  //主静态方法完
+  //*******************************************************************************************************************
+
+//min
+sas.Min = function(tasks, ite, end, opts) {
+  this.tasks_count = 0;
+  this.tasks_count_cb = 0;
+  this.STOP = false;
+  this.error = null;
+  this.end = end;
+  this.ite = ite;
+  this.process = opts.process;
+  this.process_interval = opts.process_interval || 1000;
+  this.plan = opts.copy ? sas.copy(tasks) : tasks;
+  this.init();
+}
+
+//min end
+
+//min 初始化
+sas.Min.prototype.init = function() {
+    switch (sas.type.call(this.plan)) {
+      case sas.OBJ: //Object
+
+        var keys = Object.keys(this.plan),
+          keys_len = keys.length,
+          _count = [keys_len, 0];
+        for (var o = 0; o < keys_len; o++) {
+          this.dis(keys[o], this.plan, _count);
+        }
+        break;
+      case sas.ARR: //Array
+
+        var _count = [this.plan.length, 0];
+        this.dis(_count[1], this.plan, _count);
+        break;
+      default:
+        return;
+    }
+  }
+  //min 初始化完
+
+//递归
+sas.Min.prototype.dis = function(i, t, count, parents) {
+    if (this.STOP) {
+      return;
+    }
+    switch (sas.type.call(t[i])) {
+
+      //Function Ctrl
+      case sas.FN:
+        this.tasks_count++;
+        this.forFn(i, t, count, parents);
+        break;
+
+        //Object Ctrl
+      case sas.OBJ:
+        var keys = Object.keys(t[i]),
+          keys_len = keys.length,
+          _count = [keys_len, 0];
+        for (var o = 0; o < keys_len; o++) {
+          //_count[0] ++;
+          this.dis(keys[o], t[i], _count, arguments);
+        }
+        break;
+
+        //Array Ctrl
+      case sas.ARR:
+        var _count = [t[i].length, 0];
+        this.dis(_count[1], t[i], _count, arguments);
+        break;
+
+      default:
+        //other Ctrl:
+        this.tasks_count++;
+        if (this.ite) {
+          t[i] = this.ite(t[i]);
+          this.forFn(i, t, count, parents);
+        } else {
+          count[1]++;
+          this.next_tick(i, t, count, parents);
+        }
+    }
+  }
+  //递归完
+  //*******************************************************************************************************************
+
+sas.Min.prototype.forFn = function(i, t, count, parents) {
+  var ext = null;
+  if (t[i].length > 1) {
+    ext = new sas.Index(i, t, count, parents, this);
+  }
+  t[i](cb,ext);
+  var self = this;
+
+  function cb(result, pream) {
+
+    //if (typeof result === 'string') {
+    switch (result) {
+      //==================魔法字==================
+      case '$STOP': //中止整个程序
+        if (self.end) {
+          self.end(pream); //国际惯例，第一个参数err.
+        }
+        return self.STOP = true;
+        break;
+      case '$THIS=': //替换掉 this
+        if (parents) {
+          parents[1][parents[0]] = pream;
+        }
+        count[1] = count[0];
+        break;
+      case '$END': //结束 this
+        count[1] = count[0];
+        break;
+      case '$HOLD': // 新加功能：2015-3-23 保持原来的。
+        count[1]++;
+        break;
+      case '$RELOAD': //重载当前任务
+        t[i] = pream || t[i];
+        self.dis(i, t, count, parents);
+        break;
+        //==================魔法字结束==================
+      default:
+        count[1]++;
+        if (arguments.length < 2) {
+          t[i] = result;
+        } else { //如果大于2的话，把arguments变成正常数组，保存
+          var result_tmp = [];
+          for (var r_i = 0, len = arguments.length; r_i < len; r_i++) {
+            result_tmp.push(arguments[r_i]);
+          }
+          t[i] = result_tmp;
+        }
+        self.next_tick(i, t, count, parents);
+    }
+    //}
+  }
+}
+
+sas.Min.prototype.next_tick = function(i, t, count, parents) {
+
+  if (count[0] === count[1]) {
+    if (parents) {
+      parents[2][1]++;
+      this.next_tick.apply(this, parents);
+    } else { //完结
+
+      if (this.end) {
+        this.end(null, this.plan); //国际惯例
+      }
+    }
+  } else {
+    if (typeof i === 'number') {
+      this.dis(count[1], t, count, parents);
+    }
+  }
+}
+
+
+
+//进度条
+sas.Min.prototype._process = function() { //over
+    if (this.process) {
+      this._t = setInterval(function() {
+        this.process(this.tasks_count, this.tasks_count_cb);
+      }, this.process_interval);
+    }
+  }
+  //进度条完
+sas.Min.prototype._end = function() { //over
+  if (this.process) {
+    clearInterval(this._t);
+    this.process(this.tasks_count, this.tasks_count_cb);
+    if (this.end) {
+      this.end(this.error, this.plan); //国际惯例
+    }
+  }
+}
+
+//*******************************************************************************************************************
+sas.Index = function(i, t, count, parents, dis) {
+  this.index = i;
+  this.path = [i];
+  this.count = count;
+  this.dis = dis;
+
+  var j = 0,
+    ps, isSP = false;
+
+  if (parents) {
+    ps = parents;
+    this.parent = parents[1];
+    this.pIndex = parents[0];
+
+    while (ps) {
+      j++;
+      if (!isSP && typeof ps[0] === 'number') {
+        this.Sparent = ps[1];
+        this.SpIndex = ps[0];
+        isSP = true;
+      }
+      this.path.splice(0, 0, ps[0]);
+      ps = ps[3];
+    }
+    /*      this.parents = function(num) {
+            if (num >= j) {
+              return;
+            }
+            ps = parents;
+            for (var x = 0; x < num;) {
+              ps = ps[3];
+            }
+            return ps;
+          }*/
+  }
+}
+
+sas.Index.prototype.fspath = function() {
+  var fspath_arr = [],
+    path_arr = this.path;
+  for (var path_i = 0, path_len = path_arr.length; path_i < path_len; path_i++) {
+    if (typeof path_arr[path_i] === 'string') {
+      fspath_arr.push(path_arr[path_i]);
+    }
+  }
+  return fspath_arr;
+}
+
+sas.Index.prototype.push = function(a) {
+    this.count[0]++;
+    if (this.parent) {
+      this.parent[this.pIndex].push(a);
+    } else { //没有父级，就是到顶了。
+      this.dis.plan.push(a);
+    }
+  }
+  //*******************************************************************************************************************
+
+//<DWDEBUG##############################
+/*sas.min.prototype.debug = {
+    C_START: Date.now(),
+    C_time: 0,
+    _color: function(c, str, b) {
+      b = b || 39;
+      if (typeof window !== 'undefined') {
+        console.log(str);
+      } else {
+        console.log('\u001b[' + c + 'm' + str + '\u001b[' + b + 'm');
+      }
+    }
+  }*/
+  //##############################DWDEBUG>
+
+if (typeof module === 'object' && typeof module.exports === 'object') {
+  module.exports = sas;
+}
